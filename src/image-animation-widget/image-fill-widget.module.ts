b6a75ed0cded6bcf56a6c84179452a263b7172f1
Nigel Milham <Nigel.Milham@softwareag.com>
2020-11-25 15:55:51 +0000

    Initial version

---
+++ src/image-animation-widget/image-fill-widget.module.ts
@@ -0,0 +1,48 @@
+/*
+* Copyright (c) 2019 Software AG, Darmstadt, Germany and/or its licensors
+*
+* SPDX-License-Identifier: Apache-2.0
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+import {CoreModule, HOOK_COMPONENTS} from "@c8y/ngx-components";
+import {NgModule} from "@angular/core";
+
+// This will import css from the styles folder (Note: will be applied globally, not scoped to the module/components)
+import '~styles/index.css';
+import {ImageFillWidget} from "./image-fill-widget.component";
+import {ImageFillWidgetConfig} from "./image-fill-widget-config.component";
+
+@NgModule({
+    imports: [
+        CoreModule
+    ],
+    declarations: [ImageFillWidget, ImageFillWidgetConfig],
+    entryComponents: [ImageFillWidget, ImageFillWidgetConfig],
+    providers: [
+        // Connect the widget to Cumulocity via the HOOK_COMPONENT injection token
+        {
+            provide: HOOK_COMPONENTS,
+            multi: true,
+            useValue: {
+                id: 'global.presales.image.fill.widget',
+                label: 'Image Fill',
+                description: 'Displays realtime fill level',
+                component: ImageFillWidget,
+                configComponent: ImageFillWidgetConfig,
+                previewImage: require("~styles/previewImage.png")
+            }
+        }
+    ],
+})
+export class ImageFillWidgetModule {}
